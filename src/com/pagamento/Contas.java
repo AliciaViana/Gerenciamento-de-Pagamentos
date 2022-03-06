93f6e1f50b8ed3c2eb41a5c8fa669f0b6571b4b3
AliciaViana <aliciasouza601@gmail.com>
2022-03-06 14:15:21 -0300

    gerenciapagamento

---
+++ src/com/pagamento/Contas.java
@@ -0,0 +1,17 @@
+package com.pagamento;
+
+import java.util.Scanner;
+
+public class Contas {
+
+	public static void main(String[] args) {
+		Salario salario = new Salario();
+		Scanner myObj = new Scanner(System.in);
+			
+		System.out.println("digite seu salario: ");
+		int salarioBruto = myObj.nextInt();
+		
+		int salarioLiquido = salario.salarioLiquido(salarioBruto);
+		System.out.println(salarioLiquido);
+	}
+}
---
+++ src/com/pagamento/Salario.java
@@ -0,0 +1,8 @@
+package com.pagamento;
+
+public class Salario {
+	public int salarioLiquido(int salarioBruto){
+		int salarioLiquido = salarioBruto - 300;
+		return salarioLiquido;	
+	}
+}
