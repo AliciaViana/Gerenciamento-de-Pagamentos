ecdb16618f683d35cb4a009b53ae430115102f2e
AliciaViana <aliciasouza601@gmail.com>
2022-03-07 20:54:19 -0300

    first commit

--- src/com/pagamento/pagamentos.java
+++ src/com/pagamento/pagamentos.java
@@ -2,7 +2,7 @@ package com.pagamento;
 
 import java.util.Scanner;
 
-public class Contas {
+public class pagamentos {
 
 	public static void main(String[] args) {
 		Salario salario = new Salario();
